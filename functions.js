92e46ed62c2dcff0685d4bfd66f7f8a0012c6e5c
Steve Benton <steve@bentonemail.uk>
2026-09-16 14:05:16 +0100

    add rest parameters and a spread operator

--- functions.js
+++ functions.js
@@ -4,3 +4,24 @@ function greetUser(greetingPrefix, userName = "user") {
 
 greetUser("Hi", "Steve");
 greetUser("Hello");
+
+function sumUp(numbers) {
+  let result = 0;
+  for (const number of numbers) {
+    result += number;
+  }
+  return result;
+}
+console.log(sumUp([1, 5, 10, 11]));
+
+function sumUpR(...numbers) {
+  let result = 0;
+  for (const number of numbers) {
+    result += number;
+  }
+  return result;
+}
+console.log(sumUpR(1, 5, 10, 11, 20));
+
+const inputNumbers = [1, 5, 10, 11, 23];
+console.log(sumUpR(...inputNumbers));
